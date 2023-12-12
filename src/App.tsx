acbfe78f69fca0464d01adf40e148d18aef095d8
Anshu Raj <50476777+anshuopinion@users.noreply.github.com>
2023-12-12 12:49:58 +0530

    Initial Animation Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 const App = () => {
-  return <div className="text-red-500">App</div>;
+  return (
+    <div className="grid place-items-center h-screen">
+      <div className="flex justify-center text-center flex-col gap-4">
+        <p>MONITOR. ALERT. AI DIAGNOSTICS REPORT.</p>
+        <h1 className=" linear__text__gradient text-4xl font-extrabold leading-10 tracking-tight  sm:text-6xl sm:leading-none md:text-8xl">
+          Get downtime <br />
+          AI Diagnostics to help you <br />
+          build better apps
+        </h1>
+        <p className="text-base text-gray-500 md:text-lg">
+          Join the mailing list for early access to the alpha release
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default App;
